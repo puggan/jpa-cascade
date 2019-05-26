533aed6e4fccde73600671bc83387a45315a9e0a
Thorbjörn Sundragon <puggan@spiro.se>
2019-05-26 13:11:01 +0200

    Entites

---
+++ src/main/java/se/puggan/jpatest/entites/Student.java
@@ -0,0 +1,116 @@
+package se.puggan.jpatest.entites;
+
+import se.puggan.jpatest.repo.StudentRepo;
+
+import javax.persistence.Entity;
+import javax.persistence.EntityManager;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.ManyToMany;
+import javax.transaction.Transactional;
+import java.util.Optional;
+import java.util.Set;
+
+@Entity
+public class Student
+{
+    //<editor-fold desc="DatabaseFields">
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+
+    @ManyToMany(mappedBy = "students")
+    private Set<Subject> subjects;
+    //</editor-fold>
+
+    //<editor-fold desc="Repo">
+    private static EntityManager manager;
+    private static StudentRepo repo;
+
+    public static void setRepo(StudentRepo r, EntityManager em)
+    {
+        if (repo == null)
+        {
+            repo = r;
+            manager = em;
+        }
+    }
+    //</editor-fold>
+
+    //<editor-fold desc="Constructors">
+    public Student()
+    {
+
+    }
+
+    public static Student findOrCreate(String newName)
+    {
+        Optional<Student> maybe = repo.findByName(newName);
+        if (maybe.isPresent())
+        {
+            return maybe.get();
+        }
+
+        Student s = new Student();
+        s.name = newName;
+        return s.save();
+    }
+    //</editor-fold>
+
+    public void connect(Subject s)
+    {
+        s.connect(this);
+    }
+
+    public void disconnet(Subject s)
+    {
+        s.disconnet(this);
+    }
+
+    public Student reload()
+    {
+        manager.refresh(this);
+        return this;
+    }
+
+    public Student save()
+    {
+        repo.saveAndFlush(this);
+        return reload();
+    }
+
+    public void delete()
+    {
+        for (Subject s : subjects)
+        {
+            s.disconnet(this);
+        }
+        save();
+        repo.delete(save());
+    }
+
+    //<editor-fold desc="Getters">
+    public int getId()
+    {
+        return id;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public Set<Subject> getSubjects()
+    {
+        return subjects;
+    }
+
+    public static StudentRepo getRepo()
+    {
+        return repo;
+    }
+    //</editor-fold>
+}
---
+++ src/main/java/se/puggan/jpatest/entites/Subject.java
@@ -0,0 +1,114 @@
+package se.puggan.jpatest.entites;
+
+import se.puggan.jpatest.repo.SubjectRepo;
+
+import javax.persistence.Entity;
+import javax.persistence.EntityManager;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.ManyToMany;
+import javax.transaction.Transactional;
+import java.util.Optional;
+import java.util.Set;
+
+@Entity
+public class Subject
+{
+    //<editor-fold desc="DatabaseFields">
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+
+    @ManyToMany
+    private Set<Student> students;
+    //</editor-fold>
+
+    //<editor-fold desc="Repo">
+    private static EntityManager manager;
+    private static SubjectRepo repo;
+
+    public static void setRepo(SubjectRepo r, EntityManager em)
+    {
+        if (repo == null)
+        {
+            repo = r;
+            manager = em;
+        }
+    }
+    //</editor-fold>
+
+    //<editor-fold desc="Constructors">
+    public Subject()
+    {
+
+    }
+
+    public static Subject findOrCreate(String name)
+    {
+        Optional<Subject> maybe = repo.findByName(name);
+        if(maybe.isPresent())
+        {
+            return maybe.get();
+        }
+
+        Subject s = new Subject();
+        s.name = name;
+        return s.save();
+    }
+    //</editor-fold>
+
+    public Subject connect(Student s)
+    {
+        students.add(s);
+        return save();
+    }
+
+    public Subject disconnet(Student s)
+    {
+        students.remove(s);
+        return save();
+    }
+
+    public Subject reload()
+    {
+        manager.refresh(this);
+        return this;
+    }
+
+    public Subject save()
+    {
+        repo.saveAndFlush(this);
+        return reload();
+    }
+
+    public void delete()
+    {
+        students.clear();
+        repo.delete(save());
+    }
+
+    //<editor-fold desc="Getters">
+    public int getId()
+    {
+        return id;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public Set<Student> getStudents()
+    {
+        return students;
+    }
+
+    public static SubjectRepo getRepo()
+    {
+        return repo;
+    }
+    //</editor-fold>
+}
